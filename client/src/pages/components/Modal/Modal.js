e3ee1d58e573ea8247de4503f4eee1d7d7558929
Samuel Mast <samuel.a.mast@gmail.com>
2018-04-11 23:12:32 -0600

    update to heroku site

--- client/src/pages/components/Modal/Modal.js
+++ client/src/pages/components/Modal/Modal.js
@@ -9,15 +9,15 @@ const Modal = props => (
     <div className="modal-right">
       <h1>{props.title}</h1>
       <br />
-      <p><strong>Links:</strong></p>
+      <p style={{fontSize:15}}><strong>Links:</strong></p>
       <div className="row">
         <a href={props.url} target="_blank"><span className="fa fa-link links"></span></a>
         <a href={props.githubUrl} target="_blank"><span className="fa fa-github links"></span></a>
       </div>
       <br />
-      <p><strong>Technologies:</strong> {props.tech}</p>
+      <p style={{fontSize:19}}><strong>Technologies:</strong> {props.tech}</p>
       <br />
-      <p><strong>Description:</strong> {props.description}</p>
+      <p style={{fontSize:19}}><strong>Description:</strong> {props.description}</p>
       <div className="close">
       <a onClick={props.close}>
         <span className="fa fa-close"></span>
