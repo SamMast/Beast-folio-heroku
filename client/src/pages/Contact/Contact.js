bbfeb208b2925e1923e6967bf8a85b2e7a13cd6f
Samuel Mast <samuel.a.mast@gmail.com>
2018-06-19 17:48:35 -0600

    updates

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -81,85 +81,81 @@ class Contact extends Component {
         </Sticky>
         <Nav background="white"/>
         <br />
-        <br />
-        <br />
         <div className="cont">
-          <form className="col-md-6 contactForm fadeIn" style={{display:"block", margin:"auto"}}>
-            <br />
-            <p className="text-center" style={{fontSize:"200%"}}><strong>Thank you for visiting Sam's Portfolio page!</strong></p>
-            <br/>
-            <p className="text-center">Please feel free to click the links to the right to access Sam's GitHub page, LinkedIn page, and send a direct email.  If you would like to contact Sam directly, please enter your information into the form below and send.  Sam will receive your message, and be in touch with you ASAP!</p>
-            
-            <br />
-            
-            <div className="form-row">
-              <div className="col-md-6">
-
-                  <div className="md-form form-group">
-                    <i className="fa fa-user prefix"></i>
-                    <input 
-                      required 
-                      type="text" 
-                      id="name" 
-                      className="form-control"
-                      value={this.state.name}
-                      name="name"
-                      onChange={this.handleInputChange}
-                      />
-                    <label htmlFor="name" className="active">Name</label>
-                  </div>
-              </div>
-              <div className="col-md-6">
-                  <div className="md-form form-group">   
-                    <i className="fa fa-envelope prefix"></i>
-                    <input 
-                      required 
-                      type="email"
-                      id="email" 
-                      className="form-control"
-                      value={this.state.email}
-                      name="email"
-                      onChange={this.handleInputChange}
-                    />
-                    <label htmlFor="email" className="active">E-mail address</label>
-                  </div>
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="col-md-12">
-                  <div className="md-form form-group">
-                      <i className="fa fa-pencil prefix"></i>
-                      <textarea
-                        required 
-                        type="text" 
-                        id="message" 
-                        className="form-control md-textarea" 
-                        rows="3"
-                        value={this.state.message}
-                        name="message"
-                        onChange={this.handleInputChange}
-
-                        ></textarea>
-                      <label htmlFor="message" className="active">Message</label>
-                  </div>
-              </div>
-            </div>
-            <br />
-            <button 
-              type="submit" 
-              className="btn btn-default btn-lg" 
-              style={{display:"block", margin:"auto"}}
-              onClick={this.handleFormSubmit}
-              >Submit Form</button>
-          </form>
+          <p className="text-center" style={{fontSize:"200%"}}><strong>Thank you for visiting Sam's Portfolio page!</strong></p>
+          <br/>
+          <p className="text-center" style={{width:"70%", marginLeft:"15%"}}>Please feel free to click the links to the right to access Sam's GitHub page, LinkedIn page, Resume, or to send an Email.</p>
           <br />
           <br />
-          <br />
+          </div>  
           <Footer />
-        </div>
       </StickyContainer>
     );
   }
 }
 
 export default Contact;
+
+// Removing Form until fixed
+
+/* <form className="col-md-6 contactForm fadeIn" style={{display:"block", margin:"auto"}}>
+<div className="form-row">
+  <div className="col-md-6">
+
+      <div className="md-form form-group">
+        <i className="fa fa-user prefix"></i>
+        <input 
+          required 
+          type="text" 
+          id="name" 
+          className="form-control"
+          value={this.state.name}
+          name="name"
+          onChange={this.handleInputChange}
+          />
+        <label htmlFor="name" className="active">Name</label>
+      </div>
+  </div>
+  <div className="col-md-6">
+      <div className="md-form form-group">   
+        <i className="fa fa-envelope prefix"></i>
+        <input 
+          required 
+          type="email"
+          id="email" 
+          className="form-control"
+          value={this.state.email}
+          name="email"
+          onChange={this.handleInputChange}
+        />
+        <label htmlFor="email" className="active">E-mail address</label>
+      </div>
+  </div>
+</div>
+<div className="form-row">
+  <div className="col-md-12">
+      <div className="md-form form-group">
+          <i className="fa fa-pencil prefix"></i>
+          <textarea
+            required 
+            type="text" 
+            id="message" 
+            className="form-control md-textarea" 
+            rows="3"
+            value={this.state.message}
+            name="message"
+            onChange={this.handleInputChange}
+
+            ></textarea>
+          <label htmlFor="message" className="active">Message</label>
+      </div>
+  </div>
+</div>
+<br />
+<button 
+  type="submit" 
+  className="btn btn-default btn-lg" 
+  style={{display:"block", margin:"auto"}}
+  onClick={this.handleFormSubmit}
+  >Submit Form</button>
+</form> */
